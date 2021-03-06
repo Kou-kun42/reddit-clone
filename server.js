00eb5671fdbe34e8a33371faab60b63247a68c75
Kou-kun42 <shinigamithekido@gmail.com>
2021-03-06 09:30:38 -0500

    Post creation and submission works

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // Require Libraries
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 // App Setup
 const app = express();
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.use(express.static("public"));
 
@@ -15,10 +24,19 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+require("./controllers/posts.js")(app);
+// Set db
+require("./data/reddit-db");
+
 // Routes
 app.get("/", (req, res) => {
   text = "Hello World";
   res.render("home", { text });
+});
+
+app.get("/posts/new", function (req, res) {
+  var currentUser = req.user;
+  return res.render("posts-new", { currentUser });
 });
 
 // Start Server
