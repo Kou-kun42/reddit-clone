d87a851b2584bd7e38648cfc7eda07490c19e9ec
Kou-kun42 <shinigamithekido@gmail.com>
2021-03-06 10:54:27 -0500

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
-const Post = require("../models/post");
+const Post = require("../models/posts");
+const User = require("../models/user");
 
 module.exports = (app) => {
   // CREATE
@@ -6,20 +7,36 @@ module.exports = (app) => {
     if (req.user) {
       var post = new Post(req.body);
 
-      post.save(function (err, post) {
-        return res.redirect(`/`);
-      });
+      post
+        .save()
+        .then((post) => {
+          return User.findById(req.user._id);
+        })
+        .then((user) => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          res.redirect(`/posts/${post._id}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     } else {
       return res.status(401); // UNAUTHORIZED
     }
   });
 
+  // INDEX
   app.get("/", (req, res) => {
     var currentUser = req.user;
-
+    // res.render('home', {});
+    console.log(req.cookies);
     Post.find({})
+      .lean()
+      .populate("author")
       .then((posts) => {
         res.render("posts-index", { posts, currentUser });
+        // res.render('home', {});
       })
       .catch((err) => {
         console.log(err.message);
@@ -27,12 +44,15 @@ module.exports = (app) => {
   });
 
   app.get("/posts/:id", function (req, res) {
+    var currentUser = req.user;
     // LOOK UP THE POST
+
     Post.findById(req.params.id)
       .lean()
       .populate("comments")
+      .populate("author")
       .then((post) => {
-        res.render("post-show", { post });
+        res.render("posts-show", { post, currentUser });
       })
       .catch((err) => {
         console.log(err.message);
@@ -41,10 +61,12 @@ module.exports = (app) => {
 
   // SUBREDDIT
   app.get("/n/:subreddit", function (req, res) {
+    var currentUser = req.user;
     Post.find({ subreddit: req.params.subreddit })
       .lean()
+      .populate("author")
       .then((posts) => {
-        res.render("posts-index", { posts });
+        res.render("posts-index", { posts, currentUser });
       })
       .catch((err) => {
         console.log(err);
